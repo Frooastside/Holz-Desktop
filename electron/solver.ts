f6e1fbf7d578682bcbaa09a400f804d7437e30d5
Simon <simon@benezan.de>
2022-05-29 16:51:10 +0200

    add solver.ts

---
+++ electron/solver.ts
@@ -0,0 +1,56 @@
+import { BrowserWindow, ipcMain, IpcMainEvent } from "electron";
+import { readFileSync } from "fs";
+import Handlebars from "handlebars";
+import { join } from "path";
+import { Browser } from "puppeteer-core";
+import pie from "puppeteer-in-electron";
+
+const solverFile = join(__dirname, "solver.handlebars");
+const solverTemplate = Handlebars.compile(readFileSync(solverFile).toString());
+
+export default async function solve(
+  browser: Browser,
+  captcha: { url: string; sitekey: string; cid: string }
+): Promise<string> {
+  return new Promise<string>(async (resolve, reject) => {
+    const window = new BrowserWindow({
+      width: 480,
+      height: 640,
+      frame: false,
+      show: true,
+      resizable: false,
+      fullscreenable: false,
+      webPreferences: {
+        preload: join(__dirname, "captcha-preload.js"),
+        sandbox: true
+      }
+    });
+    const page = await pie.getPage(browser as any, window);
+    await page.setRequestInterception(true);
+
+    captcha.url = captcha.url;
+
+    page.on("request", (request) => {
+      if (request.url() === captcha.url && request.isNavigationRequest()) {
+        request.respond({
+          body: solverTemplate({ sitekey: captcha.sitekey })
+        });
+      } else {
+        request.continue();
+      }
+    });
+
+    await page.goto(captcha.url, { waitUntil: "networkidle0" });
+
+    const timeout = setTimeout(() => {
+      window.close();
+      reject("Captcha Timeout.");
+    }, 120000);
+
+    ipcMain.once("result", (_event: IpcMainEvent, token: string) => {
+      clearTimeout(timeout);
+      window.close();
+      resolve(token);
+    });
+  });
+}
